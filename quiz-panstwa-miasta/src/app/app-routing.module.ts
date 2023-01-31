9275f8e33335ef1edfe1a202cc4d304d8679799c
Kasia28-jk <kasian1999@interia.pl>
2023-01-31 11:11:21 +0100

    routing

--- quiz-panstwa-miasta/src/app/app-routing.module.ts
+++ quiz-panstwa-miasta/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent},
+  { path: 'register', component: RegisterPageComponent},
+];
 
+export const routing = RouterModule.forRoot(routes, { enableTracing: true })
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
